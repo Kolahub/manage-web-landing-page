64bf2f6e80c1daec654e598dd5f61e052480c267
Ayofe Faheez <ayofefaheez@gmail.com>
2024-05-22 16:46:06 +0100

    fixed images that are not showing on mobile

--- src/components/Feedback/feedback.js
+++ src/components/Feedback/feedback.js
@@ -7,21 +7,21 @@ export const feedbackData = [
   },
 
   {
-    imgUrl: "../src/assets/images/avatar-ali.png",
+    imgUrl: "https://images.pexels.com/photos/4347368/pexels-photo-4347368.jpeg",
     name: "Ali Bravo",
     comment:
       "“We have been able to cancel so many other subscriptions since using  Manage. There is no more cross-channel confusion and everyone is much  more focused.”",
   },
 
   {
-    imgUrl: "../src/assets/images/avatar-richard.png",
+    imgUrl: "https://images.pexels.com/photos/32976/pexels-photo.jpg",
     name: "Richard Watts",
     comment:
       "“Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!”",
   },
 
   {
-    imgUrl: "../src/assets/images/avatar-shanai.png",
+    imgUrl: "https://images.pexels.com/photos/1181686/pexels-photo-1181686.jpeg",
     name: "Shanai Gough",
     comment:
       "“Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.”",
